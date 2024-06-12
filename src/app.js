6f568d35d612ad450173d3ac3932ee640f7f9adf
tuananh <tuananh.work2409@gmail.com>
2024-06-12 11:37:40 +0700

    fix cors #14

--- src/app.js
+++ src/app.js
@@ -29,10 +29,17 @@ const app = express();
 app.use(corsPreflight);
 app.use(corsConfig);
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.status(200);
     next();
 });
 initializeApp(firebaseConfig);
