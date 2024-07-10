06b16049bbf5e9c40464ec9f6c969bb5016eaf8d
minhnn83vn <minhnn83vn@gmail.com>
2024-07-10 19:16:10 +0700

    update create emoji

--- src/api/v1/routes/emoji.route.js
+++ src/api/v1/routes/emoji.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const EmojiController = require('../controllers/emoji.controller');
 
-router.post('/emojis', EmojiController.createEmoji);
+router.post('/add/emojis', EmojiController.createEmoji);
 router.get('/emojis', EmojiController.getAllEmojis);
 router.get('/emojis/:id', EmojiController.getEmojiById);
 router.put('/emojis/:id', EmojiController.updateEmoji);
