5c54b67e007ff796fe7ebdd71348b30e05fed2a7
tuananh <tuananh.work2409@gmail.com>
2024-07-19 03:17:10 +0700

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN if [ "$NODE_ENV" = "development" ]; \
 
 RUN if [ "$NODE_ENV" = "development" ]; \
         then npm install; \
-        else npm install --only=production; \
+        else npm install --omit=dev; \
         fi
 
 COPY . .
--- src/api/v1/services/mail.service.js
+++ src/api/v1/services/mail.service.js
@@ -8,7 +8,7 @@ const sendResetPasswordTokenMail = (user) => {
         },
         to: [user.email],
         subject: "Reset Mật Khẩu",
-        html: `<p>Hình như mày quên mịa mất mật khẩu. Nhấp vào link sau để reset mật khẩu nhé.</p>
+        html: `<p>Hình như bạn quên mịa mất mật khẩu. Nhấp vào link sau để reset mật khẩu nhé.</p>
             <a href="${process.env.CLIENT_URL}/reset-password/${user.passwordResetToken}">Reset Password</a>
             <p>Nên nhớ nó sẽ hết hạn vào 5 phút nữa thôi đấy</p>`,
     });
@@ -21,8 +21,8 @@ const sendVerificationMail = (user) => {
             address: process.env.MAIL_FROM_ADDRESS,
         },
         to: [user.email],
-        subject: "Xác thực Email Hộ tao cái",
-        html: `<p>Hình như mày vừa đăng kí với hệ thống của tao. Hãy xác thực hộ tao.</p>
+        subject: "Xác thực Email",
+        html: `<p>Hình như bạn vừa đăng kí với hệ thống của Chat App. Hãy xác thực ngay nhé.</p>
             <a href="${process.env.CLIENT_URL}/verify-email/${user.verificationToken}">Verify Your Email</a>`,
     });
 };
