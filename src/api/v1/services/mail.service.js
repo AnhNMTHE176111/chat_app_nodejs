e96a7d758477e1c5c5219954f6b368a17046f1ba
tuananh <tuananh.work2409@gmail.com>
2024-07-20 09:34:52 +0700

    fix mail

--- src/api/v1/services/mail.service.js
+++ src/api/v1/services/mail.service.js
@@ -8,7 +8,7 @@ const sendResetPasswordTokenMail = (user) => {
         },
         to: [user.email],
         subject: "Reset Mật Khẩu",
-        html: `<p>Hình như bạn quên mịa mất mật khẩu. Nhấp vào link sau để reset mật khẩu nhé.</p>
+        html: `<p>Hình như bạn vừa kích hoạt đặt lại mất mật khẩu. Nhấp vào link sau để reset mật khẩu nhé.</p>
             <a href="${process.env.CLIENT_URL}/reset-password/${user.passwordResetToken}">Reset Password</a>
             <p>Nên nhớ nó sẽ hết hạn vào 5 phút nữa thôi đấy</p>`,
     });
--- src/config/cookieOption.config.js
+++ src/config/cookieOption.config.js
@@ -3,7 +3,8 @@ const { CLIENT_URL } = require("../api/v1/helpers/const");
 const cookieOption = {
     httpOnly: true,
     signed: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV == "production",
+    sameSite: process.env.NODE_ENV == "production" ? "none" : "strict",
 };
 
 module.exports = cookieOption;
